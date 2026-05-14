05be1179e2fbe28ab55631030eac3b8e5dcb41de
mbunabojohn-glitch <mbunabojohn@gmail.com>
2026-05-14 08:10:03 +0100

    feat: add tech buzz section, google analytics, vercel routing fix

--- src/components/TechBuzz.tsx
+++ src/components/TechBuzz.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNews } from "../hooks/useApi";
 import { ExternalLink, Calendar, AlertCircle, Newspaper } from "lucide-react";
 
